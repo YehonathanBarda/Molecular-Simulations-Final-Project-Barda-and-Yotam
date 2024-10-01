f1aeb2c3ec6f02b447b727ffcfc5941460b34374
Yehonathan Barda <yehonathan.barda@gmail.com>
2024-10-02 01:18:29 +0300

    test

--- test.py
+++ test.py
@@ -2,10 +2,14 @@ import numpy as np
 R = np.array([[1,0,0],[2,0,0],[3,0,0],[4,0,0],[5,0,0]])
 print(R)
 print('\n_____________________________________\n')
-a = np.arange(5)
-print(a)
-print('\n_____________________________________\n')
-a2 = a[:, np.newaxis]
-print(a2)
-print('\n_____________________________________\n')
-print(a2 * R)
+avg = np.mean(R, axis=0)
+print(avg)
+
+
+# a = np.arange(5)
+# print(a)
+# print('\n_____________________________________\n')
+# a2 = a[:, np.newaxis]
+# print(a2)
+# print('\n_____________________________________\n')
+# print(a2 * R)
